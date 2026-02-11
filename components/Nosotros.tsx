14e459ccf437c005bf56ac2fdc29bfac240fc149
chapsMX <72465656+chapsMX@users.noreply.github.com>
2026-02-11 12:10:47 -0600

    refactor Nosotros
    Updated padding and logo image properties in Nosotros component.

--- components/Nosotros.tsx
+++ components/Nosotros.tsx
@@ -5,15 +5,15 @@ import Image from 'next/image'
 export default function Nosotros() {
   return (
     <section className="bg-c13-dark">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         {/* Header with Logo */}
-        <div className="text-center mb-16">
-          <div className="flex justify-center mb-8">
+        <div className="text-center mb-4">
+          <div className="flex justify-center mb-0">
             <Image
-              src="/images/logo.png"
+              src="/images/c13studioLogot.png"
               alt="c13studio"
-              height={200}
-              width={200}
+              height={250}
+              width={250}
               className="object-contain"
             />
           </div>
