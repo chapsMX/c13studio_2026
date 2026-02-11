88abc175603dbd9898ae8b669bb540ca7777de82
chapsMX <72465656+chapsMX@users.noreply.github.com>
2026-02-10 19:45:56 -0600

    Fix import case for Google analytics module

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
-import analiticas from "./Google";
+import analiticas from "./google";
 
 const poppins = Poppins({
   subsets: ['latin'],
